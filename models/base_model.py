e6c5c37e753fb415745a0ce17cd3661f0473da5d
OmarKHDR <omarsliman37@gmail.com>
2024-07-26 05:56:07 +0300

    mistacked

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,15 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = dt.isoformat(dt.now())
-        self.updated_at = dt.isoformat(dt.now())
+        self.created_at = (dt.now())
+        self.updated_at = (dt.now())
 
     def __str__(self):
         nm = f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
         return nm
 
     def save(self):
-        self.updated_at = dt.isoformat(dt.now())
+        self.updated_at = (dt.now())
 
     def to_dict(self):
         dic = self.__dict__.copy()
