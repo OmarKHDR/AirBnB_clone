3198a05cde66eef2cd3bf94f027c34e95f4900a2
OmarKHDR <omarsliman37@gmail.com>
2024-07-26 05:49:57 +0300

    save method updates the updated_at property now and to_dict almost returning class dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,21 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = dt.now()
-        self.updated_at = dt.now()
+        self.created_at = dt.isoformat(dt.now())
+        self.updated_at = dt.isoformat(dt.now())
 
     def __str__(self):
         nm = f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
         return nm
+
+    def save(self):
+        self.updated_at = dt.isoformat(dt.now())
+
+    def to_dict(self):
+        dic = self.__dict__.copy()
+        dic['__class__'] = type(self).__name__
+        return dic
+
+
+a = BaseModel()
+print(a.updated_at)
