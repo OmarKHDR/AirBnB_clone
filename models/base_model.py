3f5a92ef31f5023ea0e94a370b3f7a95844b1187
OmarKHDR <omarsliman37@gmail.com>
2024-07-26 10:27:41 +0300

    [A[A[B[B[C[D

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,10 @@ class BaseModel:
                     self.key = dt.strptime(kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
                     continue
                 self.key = kwargs[key]
-            return
-        self.id = str(uuid.uuid4())
-        self.created_at = (dt.now())
-        self.updated_at = (dt.now())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = (dt.now())
+            self.updated_at = (dt.now())
 
     def __str__(self):
         nm = f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
