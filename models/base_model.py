9a14ea5fa3c082f38127caf611f93c1f6d7d4f68
OmarKHDR <omarsliman37@gmail.com>
2024-07-26 10:40:45 +0300

    only gods knows what has happened here]]]]]]]]]]]]]]]]

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """base class with basic functionality"""
 import uuid
 from datetime import datetime as dt
+timeformat = '%Y-%m-%dT%H:%M:%S.%f'
 
 
 class BaseModel:
@@ -10,10 +11,13 @@ class BaseModel:
         if len(kwargs) > 0:
             for key in kwargs.keys():
                 if key == 'created_at' or key == 'updated_at':
-                    self.key = dt.strptime(kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, dt.strptime(kwargs[key], timeformat))
                     continue
-                self.key = kwargs[key]
+                if key == '__class__':
+                    continue
+                setattr(self, key, kwargs[key])
         else:
+            print("idiot")
             self.id = str(uuid.uuid4())
             self.created_at = (dt.now())
             self.updated_at = (dt.now())
@@ -24,6 +28,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = (dt.now())
+
     def to_dict(self):
         dic = self.__dict__.copy()
         dic['updated_at'] = dt.isoformat(self.updated_at)
