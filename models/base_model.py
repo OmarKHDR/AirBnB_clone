b9b9cebd1d1f2639a5b376d67b08126c09d52f96
OmarKHDR <omarsliman37@gmail.com>
2024-07-27 14:13:32 +0300

    hello

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,8 @@
 """base class with basic functionality"""
 import uuid
 from datetime import datetime as dt
-from models import storage
+import models
+#from models import models.storage
 
 
 timeformat = '%Y-%m-%dT%H:%M:%S.%f'
@@ -24,14 +25,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = (dt.now())
             self.updated_at = (dt.now())
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         nm = f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
         return nm
 
     def save(self):
-        storage.save()
+        models.storage.save()
         self.updated_at = (dt.now())
 
     def to_dict(self):
